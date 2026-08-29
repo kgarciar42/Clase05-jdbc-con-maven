d7c78828430b46c6b6ae349929898393df7a0767
Kervin Garcia <kgarciar42@miumg.edu.gt>
2026-08-28 22:31:05 -0600

    Se agregaron campos activo, tipo y email a Estudiante

--- src/main/java/edu/umg/programacion2/clase05/modelo/Estudiante.java
+++ src/main/java/edu/umg/programacion2/clase05/modelo/Estudiante.java
@@ -11,20 +11,35 @@ package edu.umg.programacion2.clase05.modelo;
  */
 public class Estudiante {
 
+	 // Constantes para no repetir estos strings en Main/EstudianteDAO
+    public static final String TIPO_PREGRADO = "Pregrado";
+    public static final String TIPO_POSTGRADO = "Postgrado";
     private int id;
     private String nombre;
     private String carnet;
+    
+    // Se Agrego 3 atributos nuevos
+    private boolean activo;
+    private String tipo;
+    private String email;
 
-    public Estudiante(int id, String nombre, String carnet) {
+    // se agregaron activo, tipo, email aqui
+    public Estudiante(int id, String nombre, String carnet, boolean activo, String tipo, String email ) {
         this.id = id;
         this.nombre = nombre;
         this.carnet = carnet;
+        
+        // se agrego estos 3
+        this.activo = activo;
+        this.tipo = tipo;
+        this.email = email;
     }
 
     // Constructor de conveniencia para cuando todavia no existe en la base de
     // datos (por eso id = 0: MySQL le va a asignar el id real al insertarlo).
-    public Estudiante(String nombre, String carnet) {
-        this(0, nombre, carnet);
+    // se agrego activo, tipo, email
+    public Estudiante(String nombre, String carnet , boolean activo, String tipo, String email) {
+        this(0, nombre, carnet, activo, tipo, email);
     }
 
     public int getId() {
@@ -43,8 +58,22 @@ public class Estudiante {
         return carnet;
     }
 
+    // se agrego activo
+    public boolean isActivo() { return activo; }
+    public void setActivo(boolean activo) { this.activo = activo; }
+
+    // se agrego tipo
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+    
+    // se agrego emial
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    // ahora incluye activo/tipo/email
     @Override
-    public String toString() {
-        return String.format("[%d] %s - carnet %s", id, nombre, carnet);
-    }
+    public String toString () {
+    return String.format("[%d] %s - carnet %s - %s - %s - %s",
+            id, nombre, carnet, (activo ? "activo" : "inactivo"), tipo, email);
+	}
 }
